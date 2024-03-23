aaa90d13eb0548074ac45dcb52ec508361f8395b
Fran Panteli <francescapanteli@gmail.com>
2024-03-23 18:37:32 +0000

    This commit pushes project files.

---
+++ git_logs/git log sample.env .sh
@@ -0,0 +1,11 @@
+camper: /sql_reference$ git log sample.env
+commit 6695051d47b42470a18e614869997af9448221b1 (HEAD -> main)
+Author: fran panteli <francescapanteli@gmail.com>
+Date:   Sat Mar 23 15:46:35 2024 +0000
+
+    feat: add .gitignore and sample.env
+    
+    feat: add .gitignore
+    
+    feat: add sample.env
+camper: /sql_reference$ 
---
+++ git_logs/git log sql_reference.json.sh
@@ -0,0 +1,47 @@
+camper: /sql_reference$ git log sql_reference.json
+commit e581e6165444a24ac641bc4af08eb0e4b1987d26
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Sep 9 22:47:21 2021 -0500
+
+    fix: add missing rename references
+    
+    fix: add missing rename database reference
+    
+    fix: add missing rename table reference
+
+commit 193e3a260a3d132a5aeb82ae7303cc499842ae12
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Sep 9 22:04:50 2021 -0500
+
+    feat: add column references
+    
+    feat: add drop column reference
+    
+    feat: add rename column reference
+    
+    feat: add primary key reference
+    
+    feat: add foreign key reference
+
+commit 87db07c94a4273df4de2aa89b756d605f558fc05
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Sep 9 22:43:17 2021 -0500
+
+    feat: add delete row reference
+
+commit a72aceaad172418b6ac8f2e0db44c6e9bc45483e
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Sep 9 22:32:31 2021 -0500
+
+    feat: add update row reference
+
+commit 88989a27ab2087f3aea1515c6f8ab4253f384a34
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Sep 9 22:15:07 2021 -0500
+
+    feat: add insert row reference
+
+commit d3609d2b8047c0d7da867b65072151d8eb69484e
+Author: moT01 <20648924+moT01@users.noreply.github.com>
+Date:   Thu Sep 9 22:08:00 2021 -0500
+:
